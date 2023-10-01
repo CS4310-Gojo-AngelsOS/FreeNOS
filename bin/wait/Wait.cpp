af8302d87a2556d1759b8b265f44c337a68392af
sanlb3 <sanlb2810@gmai.com>
2023-09-30 19:54:42 -0700

    Update Wait.cpp

--- bin/wait/Wait.cpp
+++ bin/wait/Wait.cpp
@@ -3,14 +3,14 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <sys/wait.h>
 #include "Wait.h"
 
 Wait::Wait(int argc, char **argv)
     : POSIXApplication(argc, argv)
 {
     parser().setDescription("Wait for a background process to finish.");
-    parser().registerPositional("PID", "Process ID of the background process to wait for.");
+    parser().registerPositional("PROCESS_ID", "Process ID of the background process to wait for.");
 }
 
 Wait::~Wait()
@@ -19,5 +19,25 @@ Wait::~Wait()
 
 Wait::Result Wait::exec()
 {
-   
+
+    int processID = 0;
+
+    // Converts process ID into integer and checks if it is valid
+    // if not valid returns error
+    if ((processID = atoi(arguments().get("PROCESS_ID"))) <= 0) {
+        
+        ERROR("Process ID Invalid" << arguments().get("PROCESS_ID") << "'");
+        return IOError;
+    }
+
+    int status; 
+
+    // Uses waitpid function to suspend execution
+    if (waitpid(processID, &status, 0) == -1) {
+
+        ERROR("Failed to wait:" << strerror(errno));
+        return IOError;
+    }
+
+    return Success; 
 }
